6f7820771f458462b174e6ff3f14eaeb711dcb08
Michael Czechowski <mail@dailysh.it>
2017-03-07 10:01:23 +0100

    basic gulp tasks implemented

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
-'use strict
+'use strict'
 
-
-const gulp = re('gulp')
+const gulp = require('gulp')
 const sass = require('gulp-sass')
 
-gulp.task('sass', function () {
-    gulp.src('./scss/*.scss')
-        .pipe(sass())
+gulp.task('styles', styles)
+gulp.task('styles:watch', watch)
+
+function styles() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
-})
+}
+
+function watch() {
+    gulp.watch('./scss/**/*.scss', ['styles'])
+}
