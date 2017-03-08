d453052ee3c6bf677eefa7f3e430b5383871de89
Michael Czechowski <mail@dailysh.it>
2017-03-08 01:02:38 +0100

    first draft working

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@
 
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const browserSync = require('browser-sync')
 
 gulp.task('styles', styles)
 gulp.task('styles:watch', watch)
+
+function reloadBrowserSync(cb) {
+  browserSync.reload()
+  cb()
+}
 
 function styles() {
     return gulp.src('./scss/**/*.scss')
